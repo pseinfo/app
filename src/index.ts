f65b5a94c7bd10848edea4bd400d6971c917f58d
komed3 <pk97@outlook.de>
2025-10-05 19:26:20 +0200

    start server

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import { Server } from '@core/Server';
+
+( async () : Promise< void > => {
+
+    try {
+
+        const server = new Server();
+        await server.start();
+
+    } catch ( err ) {
+
+        console.error( 'Failed to start server:', err );
+        process.exit( 1 );
+
+    }
+
+} )();
