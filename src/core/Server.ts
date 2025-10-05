74627d7832ec2325ace806795e8abaec0da1df1e
komed3 <pk97@outlook.de>
2025-10-05 19:42:25 +0200

    create Server class

--- src/core/Server.ts
+++ src/core/Server.ts
@@ -1,1 +1,17 @@
-export class Server {}
+import express, { Application } from 'express';
+
+export class Server {
+
+    private readonly expressApp: Application;
+
+    public get app () : Application { return this.expressApp }
+
+    constructor () {
+
+        this.expressApp = express();
+
+    }
+
+    public async start () : Promise< void > {}
+
+}
