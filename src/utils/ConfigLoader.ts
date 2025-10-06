295f4a2903115df4e7f78da0f59e375a8bccc22c
komed3 <pk97@outlook.de>
2025-10-06 12:26:53 +0200

    public getters

--- src/utils/ConfigLoader.ts
+++ src/utils/ConfigLoader.ts
@@ -6,7 +6,6 @@ import {} from 'js-yaml';
 export class ConfigLoader {
 
     private readonly _configDir: string;
-
     private _env: string;
     private _config: ServerConfig;
     private _isLoaded: boolean = false;
@@ -14,9 +13,17 @@ export class ConfigLoader {
     constructor ( env?: string, configDir?: string ) {
 
         this._configDir = configDir || join( process.cwd(), 'config' );
-
         this._env = env || process.env.NODE_ENV || 'production';
         this._config = {} as ServerConfig;
+
+    }
+
+    public get env () : string { return this._env }
+
+    public get cfg () : ServerConfig {
+
+        if ( ! this._isLoaded ) throw new Error ( 'Configuration not loaded. Call loadConfig() first.' );
+        return this._config;
 
     }
 
