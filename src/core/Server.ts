831becab1a320d104f904cef4cbaedf29eca5a47
komed3 <pk97@outlook.de>
2025-10-06 14:16:12 +0200

    create server class

--- src/core/Server.ts
+++ src/core/Server.ts
@@ -1,1 +1,29 @@
-export class Server {}
+import { ConfigLoader } from '@pseinfo/app/core/ConfigLoader';
+import { Router } from '@pseinfo/app/core/Router';
+import express, { Application } from 'express';
+
+export class Server {
+
+    private config: ConfigLoader;
+    private router: Router;
+    private expressApp: Application;
+
+    constructor () {
+
+        this.config = new ConfigLoader();
+        this.router = new Router();
+        this.expressApp = express();
+
+    }
+
+    public async init () : Promise< void > {
+
+        await this.config.loadConfig();
+
+    }
+
+    public start () : void {}
+
+    public stop () : void {}
+
+}
