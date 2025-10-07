7844c00b3db8723e845b53c9a4744171321f59ae
komed3 <pk97@outlook.de>
2025-10-07 09:25:45 +0200

    implement router class

--- src/core/Router.ts
+++ src/core/Router.ts
@@ -1,1 +1,28 @@
-export class Router {}
+import { BaseController } from '@pseinfo/app/controller/BaseController';
+import { Router as ExpressRouter } from 'express';
+
+export class Router {
+
+    private readonly _router: ExpressRouter;
+
+    public get router () : ExpressRouter { return this._router }
+
+    constructor () { this._router = ExpressRouter() }
+
+    public registerController ( controller: BaseController ) : void {
+
+        const route = controller.getRoute();
+        const handler = controller.handle.bind( controller );
+
+        this.router.get( route, handler );
+        this.router.post( route, handler );
+
+    }
+
+    public registerControllers ( controllers: BaseController[] ) : void {
+
+        for ( const controller in controllers ) this.registerController( controller );
+
+    }
+
+}
