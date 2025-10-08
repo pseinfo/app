043ece299acfd2f4a0e5d9ef403bda4ab18313e8
komed3 <pk97@outlook.de>
2025-10-08 22:21:56 +0200

    now after tests are over, restarting the project

--- src/controller/HomeController.ts
+++
@@ -1,7 +0,0 @@
-import { PageController } from '@pseinfo/app/controller/PageController';
-
-export class HomeController extends PageController {
-
-    constructor () { super( { route: '{/}', template: 'home' } ) }
-
-}
--- src/controller/index.ts
+++
@@ -1,6 +0,0 @@
-import { BaseController } from '@pseinfo/app/controller/BaseController';
-import { HomeController } from '@pseinfo/app/controller/HomeController';
-
-export const controller: BaseController[] = [
-    new HomeController()
-];
