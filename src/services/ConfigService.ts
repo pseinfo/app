732ce9d0fff3e375fee21c27e88bd6fa660c4700
komed3 <pk97@outlook.de>
2025-10-08 23:46:31 +0200

    cfg service constructor

--- src/services/ConfigService.ts
+++ src/services/ConfigService.ts
@@ -1,8 +1,26 @@
 import { ServerConfig } from '@pseinfo/app/types/index';
-import { IConfig } from '@pseinfo/app/types/interfaces';
+import { IConfig, ILogger } from '@pseinfo/app/types/interfaces';
 import { access, readFile } from 'node:fs/promises';
 import { join } from 'node:path';
 import deepmerge from 'deepmerge';
 import { load } from 'js-yaml';
 
-export class ConfigurationService implements IConfig {}
+export class ConfigurationService implements IConfig {
+
+    private readonly _configDir: string;
+    private readonly _encoding: BufferEncoding;
+    private _env: string;
+    private _config: ServerConfig;
+    private _isLoaded: boolean = false;
+    private _cache: Map< string, any > = new Map();
+
+    constructor ( private logger: ILogger, env?: string, configDir?: string, encoding: BufferEncoding = 'utf8' ) {
+
+        this._configDir = configDir || join( process.cwd(), 'config' );
+        this._encoding = encoding;
+        this._env = env || process.env.NODE_ENV || 'production';
+        this._config = {} as ServerConfig;
+
+    }
+
+}
