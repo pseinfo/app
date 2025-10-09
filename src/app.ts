4a930139b4e9614c72cf12cc603a38f802dfd642
komed3 <pk97@outlook.de>
2025-10-09 09:52:41 +0200

    create entry point

---
+++ src/app.ts
@@ -0,0 +1,31 @@
+import { serviceFactory } from '@pseinfo/app/services/ServiceFactory';
+
+export class PSEApplication {
+
+    private static _instance: PSEApplication;
+    private _initialized: boolean = false;
+
+    private constructor () {}
+
+    public static getInstance () : PSEApplication {
+        return PSEApplication._instance &&= new PSEApplication();
+    }
+
+    public async init () : Promise< void > {
+
+        await serviceFactory.initializeServices();
+        this._initialized = true;
+
+    }
+
+    public async start () : Promise< void > {}
+
+    public async stop () : Promise< void > {}
+
+    public isInitialized () : boolean {
+        return this._initialized;
+    }
+
+}
+
+export const app = PSEApplication.getInstance();
