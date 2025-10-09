273037f0e440e4eeba74cb17f1be0a3d81412f70
komed3 <pk97@outlook.de>
2025-10-09 08:26:40 +0200

    Update Server.ts

--- src/services/Server.ts
+++ src/services/Server.ts
@@ -1,3 +1,15 @@
 import { IServer } from '@pseinfo/app/types/interfaces';
+import { Server as HttpServer } from 'node:http';
+import express, { Application } from 'express';
 
-export class Server implements IServer {}
+export class Server implements IServer {
+
+    private _app: Application;
+    private _server?: HttpServer;
+    private _isRunning: boolean = false;
+
+    constructor () {
+        this._app = express();
+    }
+
+}
