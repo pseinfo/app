f3637cd243fce159a254bb3aa447340b3cc6c181
komed3 <pk97@outlook.de>
2026-05-24 11:27:56 +0200

    setup postcss / tailwind

---
+++ tailwind.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from 'tailwindcss';
+
+export default {
+  content: [
+    './src/**/*.{astro,html,js,jsx,ts,tsx,md,mdx}'
+  ],
+
+  theme: {
+    extend: {}
+  },
+
+  plugins: []
+} satisfies Config;
