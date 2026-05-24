671460e787088ca2367f366b9c848f623aecac81
komed3 <pk97@outlook.de>
2026-05-24 03:00:30 +0200

    setup dependabot

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,4 @@ import { App } from '@/App';
 import '@/styles.css';
 
 const app = createRoot( document.getElementById( 'app' )! );
-
-app.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+app.render( <StrictMode> <App /> </StrictMode> );
