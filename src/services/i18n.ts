2e94b271910414c6ccaef4de3b0a483d119597df
komed3 <pk97@outlook.de>
2026-05-25 10:38:56 +0200

    setup i18next service

---
+++ src/services/i18n.ts
@@ -0,0 +1,10 @@
+import i18nInit from '@pseinfo/i18n/config/app';
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+
+i18n
+  .use( initReactI18next )
+  .init( i18nInit.init );
+
+export default i18n;
