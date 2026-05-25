0f26ebfdf7c03abfe4a81b4b52ae201be1c6ee48
komed3 <pk97@outlook.de>
2026-05-25 18:55:18 +0200

    create main header component

---
+++ src/components/layout/header/index.tsx
@@ -0,0 +1,25 @@
+import type { JSX } from 'react';
+
+
+export const Header = () : JSX.Element => {
+  return (
+    <header
+      id= 'header'
+      role= 'banner'
+      aria-label= 'Header'
+      className= {
+        'fixed top-0 left-0 z-50 w-full h-16 bg-white shadow-sm ' +
+        'shadow-slate-300/50 select-none'
+      }
+    >
+      <div
+        className= 'flex justify-between items-center gap-4 h-full px-3 sm:px-4'
+      >
+        { /** LEFT SIDE */ }
+        <div
+          className= 'flex items-center min-w-0'
+        />
+      </div>
+    </header>
+  );
+};
