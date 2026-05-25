91e459a81f1530e9efe6d8890ed59b788dabbdba
komed3 <pk97@outlook.de>
2026-05-25 14:54:05 +0200

    add config files for tailwind and postcss

---
+++ tailwind.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from 'tailwindcss';
+
+
+export default ( {
+  content: [
+    './src/**/*.{astro,html,js,jsx,ts,tsx,md,mdx}'
+  ],
+
+  theme: {
+    extend: {}
+  },
+
+  plugins: []
+} ) as const satisfies Config;
