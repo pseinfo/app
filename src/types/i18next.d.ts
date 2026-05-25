ab3258a20f56a7f96c997b54525a66e270483d3c
komed3 <pk97@outlook.de>
2026-05-25 20:01:30 +0200

    bug fix

--- src/types/i18next.d.ts
+++ src/types/i18next.d.ts
@@ -2,7 +2,7 @@ import type { AppDefaultNS, AppI18nResources } from '@pseinfo/i18n/config/app';
 import 'i18next';
 
 
-declare module i18next {
+declare module 'i18next' {
   interface CustomTypeOptions {
     defaultNS: AppDefaultNS;
     enableSelector: true;
