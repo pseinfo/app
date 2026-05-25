dbb8c039b03755e95c015f01a0a24879a033fc4d
komed3 <pk97@outlook.de>
2026-05-25 15:00:46 +0200

    resolve paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import react from '@astrojs/react';
 import { defineConfig } from 'astro/config';
 
 
+const getAliasUrl = ( path ) => new URL( path, import.meta.url ).pathname;
+
 export default defineConfig( {
   site: 'https://pse-info.de',
   output: 'static',
@@ -12,6 +14,24 @@ export default defineConfig( {
   integrations: [
     react()
   ],
+
+  vite: {
+    resolve: {
+      alias: {
+        '@': getAliasUrl( './src' ),
+        '@/asset': getAliasUrl( './src/assets' ),
+        '@/ctx': getAliasUrl( './src/context' ),
+        '@/hook': getAliasUrl( './src/hooks' ),
+        '@/layout': getAliasUrl( './src/components/layout' ),
+        '@/lib': getAliasUrl( './src/lib' ),
+        '@/page': getAliasUrl( './src/pages' ),
+        '@/provider': getAliasUrl( './src/providers' ),
+        '@/service': getAliasUrl( './src/services' ),
+        '@/type': getAliasUrl( './src/types' ),
+        '@/ui': getAliasUrl( './src/components/ui' )
+      }
+    }
+  },
 
   build: {
     format: 'directory',
